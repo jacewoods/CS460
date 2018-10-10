6eb5b4f111cc3153f3a173e02f1e14eb6db57a54
Jace Woods <jwoods17@mail.wou.edu>
2018-10-10 00:17:12 -0700

    changed name of c# project

--- homework3/generateBinary/generateBinary/Properties/AssemblyInfo.cs
+++ homework3/generateBinary/generateBinary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("generateBinary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("generateBinary")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b20a1759-605b-4e65-9a66-83453c523b4e")]
+[assembly: Guid("6a616808-7713-4748-84f8-e5fdd980ae2b")]
 
 // Version information for an assembly consists of the following four values:
 //
