a97bfacdf0387fd4f33f6d0cf6ebcaca44d43be9
Jace Woods <jwoods17@mail.wou.edu>
2018-10-11 22:59:05 -0700

    added notes to QueueUnderflowException.cs, created LinkedQueue.cs

--- homework3/generateBinary/generateBinary/QueueUnderflowException.cs
+++ homework3/generateBinary/generateBinary/QueueUnderflowException.cs
@@ -6,9 +6,30 @@ using System.Threading.Tasks;
 
 namespace generateBinary
 {
-    public class QueueUnderflowException
+    /// <summary>
+    /// Exception that can catch situations where
+    /// an illegal operation was performed on an empty queue.
+    /// 
+    /// QueueUnderflowException class extends SystemException
+    /// </summary>
+    class QueueUnderflowException : SystemException
     {
-        //base :();
+        /// <summary>
+        /// Overrides the SystemException using base()
+        /// </summary>
+        public QueueUnderflowException() : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Overrides the SystemException using base()
+        /// </summary>
+        /// <param name="message"> Receives a message and overrides SystemException using base(message) </param>
+        public QueueUnderflowException(string message) : base(message)
+        {
+
+        }
         
     }
 }
