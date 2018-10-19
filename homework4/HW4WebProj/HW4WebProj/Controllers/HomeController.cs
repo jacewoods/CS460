41c6462e603c6c2c78437a69906689b6b6990bc0
Jace Woods <jwoods17@mail.wou.edu>
2018-10-19 01:51:31 -0700

    converter page notes completed and fully functional

--- homework4/HW4WebProj/HW4WebProj/Controllers/HomeController.cs
+++ homework4/HW4WebProj/HW4WebProj/Controllers/HomeController.cs
@@ -32,39 +32,47 @@ namespace HW4WebProj.Controllers
             string miles = Request.QueryString["mileInput"];
             //Requests the metric unit input by user into "metric"
             string metric = Request.QueryString["chooseMetric"];
+            //Initializes input
             double mileVal = 0;
 
+            //Makes sure no conversion is posted on page startup
             if (miles != null)
             {
-                mileVal = Convert.ToDouble(miles);
-                if (metric == "mm")
+                //Ensures conversion to double is only done if there is an input in "Miles"
+                if (miles != "")
+                {
+                    mileVal = Convert.ToDouble(miles);
+                }
+                else
+                {
+                    return View();
+                }
+
+                //Does conversion based on metric input
+                if (metric == "millimeters")
                 {
                     mileVal = mileVal * 1609344.0;
                 }
-                else if (metric == "cm")
+                else if (metric == "centimeters")
                 {
                     mileVal = mileVal * 160934.4;
                 }
-                else if (metric == "m")
+                else if (metric == "meters")
                 {
                     mileVal = mileVal * 1609.344;
                 }
-                else if (metric == "km")
+                else if (metric == "kilometers")
                 {
                     mileVal = mileVal * 1.609344;
                 }
 
+                //Converts the resulting number to a string for output
                 string outputNum = mileVal.ToString();
 
-                ViewBag.message = outputNum;
+                //Outputs final message after conversion is complete
+                ViewBag.message = miles + " miles is equal to " + outputNum + " " + metric;
             }
-
-
-
-
-
             return View();
-
         }
     }
 }
