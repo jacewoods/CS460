fced1368e0cb469da184e3279bdafba8d0d760cb
Jace Woods <jwoods17@mail.wou.edu>
2018-10-19 15:54:57 -0700

    foundation for /Color page added

--- homework4/HW4WebProj/HW4WebProj/Controllers/ColorController.cs
+++ homework4/HW4WebProj/HW4WebProj/Controllers/ColorController.cs
@@ -9,15 +9,20 @@ namespace HW4WebProj.Controllers
     public class ColorController : Controller
     {
         // GET: Color
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Message = "Enter colors you wish to mix in HTML hexadecimal format. Ex: #AABBCC";
-
+            ViewBag.show = false;
             return View();
         }
 
-        public ActionResult Color()
+        [HttpPost]
+        public ActionResult Color(string firstColor, string secondColor)
         {
+            firstColor = Request.Form["colorOne"];
+            secondColor = Request.Form["colorTwo"];
+
             return View();
         }
     }
