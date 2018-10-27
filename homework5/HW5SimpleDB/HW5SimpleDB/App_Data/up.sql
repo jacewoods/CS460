00c02cfd16badb4be5dba25b73c4778ea39fd130
Jace Woods <jwoods17@mail.wou.edu>
2018-10-27 05:31:10 -0700

    stylized the tennant request form

--- homework5/HW5SimpleDB/HW5SimpleDB/App_Data/up.sql
+++ homework5/HW5SimpleDB/HW5SimpleDB/App_Data/up.sql
@@ -13,9 +13,9 @@ CREATE TABLE [dbo].[Renters]
 );
 
 INSERT INTO [dbo].[Renters] (FirstName, LastName, PhoneNumber, ApartmentName, UnitNumber, Explanation, Permission) VALUES
-	('Sam', 'Samson', '503-444-5555', 'Westfield Apartments', '280', 'Fridge Exploded', 0),
+	('Sam', 'Samson', '503-444-5555', 'Westfield Apartments', '280', 'Freezer Exploded', 0),
 	('John', 'Roberts', '971-888-8889', 'Clearwater Apartments', '300', 'Bathroom door split in half', 1),
-	('Theresa', 'King', '503-999-9990', 'Ridgeview Apartments', '1002', 'Fridge ate my Homework', 0),
+	('Theresa', 'King', '503-999-9990', 'Ridgeview Apartments', '1002', 'The pool water is green', 0),
 	('Jane', 'Thomas', '971-111-1112', 'Ridgeview Apartments', '80', 'Washing Machine is giving attitude', 1),
 	('Bill', 'Smith', '503-333-3336', 'Westfield Apartments', '85', 'The Carpet has been stolen', 1)
 GO
