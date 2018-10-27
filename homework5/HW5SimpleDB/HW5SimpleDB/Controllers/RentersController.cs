7b55c5704c290a6375d518d9307f17aa1f732ffe
Jace Woods <jwoods17@mail.wou.edu>
2018-10-27 01:23:56 -0700

    deleted unnecessary files, made changes to style of request table

--- homework5/HW5SimpleDB/HW5SimpleDB/Controllers/RentersController.cs
+++ homework5/HW5SimpleDB/HW5SimpleDB/Controllers/RentersController.cs
@@ -21,20 +21,6 @@ namespace HW5SimpleDB.Controllers
             return View(db.Renters.ToList());
         }
 
-        // GET: Renters/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Renter renter = db.Renters.Find(id);
-            if (renter == null)
-            {
-                return HttpNotFound();
-            }
-            return View(renter);
-        }
 
         // GET: Renters/Create
         public ActionResult Create()
@@ -42,12 +28,9 @@ namespace HW5SimpleDB.Controllers
             return View();
         }
 
-        // POST: Renters/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,PhoneNumber,ApartmentName,UnitNumber,Explanation,Permission")] Renter renter)
+        public ActionResult Create(Renter renter)
         {
             if (ModelState.IsValid)
             {
@@ -55,74 +38,7 @@ namespace HW5SimpleDB.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
             return View(renter);
-        }
-
-        // GET: Renters/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Renter renter = db.Renters.Find(id);
-            if (renter == null)
-            {
-                return HttpNotFound();
-            }
-            return View(renter);
-        }
-
-        // POST: Renters/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,PhoneNumber,ApartmentName,UnitNumber,Explanation,Permission")] Renter renter)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(renter).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(renter);
-        }
-
-        // GET: Renters/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Renter renter = db.Renters.Find(id);
-            if (renter == null)
-            {
-                return HttpNotFound();
-            }
-            return View(renter);
-        }
-
-        // POST: Renters/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Renter renter = db.Renters.Find(id);
-            db.Renters.Remove(renter);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
