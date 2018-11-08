18c15c6eaf7df5b08b4fa7f25d61421253bb6d77
Jace Woods <jwoods17@mail.wou.edu>
2018-11-08 02:02:35 -0800

    returns test message on spacebar click

---
+++ homework7/GiphyProj/GiphyProj/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiphyProj.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public JsonResult HelloWorld()
+        {
+            var data = new
+            {
+                message = "Hello",
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ homework7/GiphyProj/GiphyProj/Scripts/helloworld.js
@@ -0,0 +1,31 @@
+﻿console.log("In hellowworld.js")
+
+
+
+$("#Request").keypress(function (e) {
+    if (e.key === ' ' || e.key === 'Spacebar') {
+        $("#message").html("Hello, World.");
+        var a = 5;
+        console.log(a);
+        var source = "/Hello/HelloWorld/" + a;
+        console.log(source);
+    }
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: source,
+        success: displayData,
+        error: errorOnAjax
+    });
+
+});
+
+function displayData(data) {
+    console.log(data);
+    $("message").text(data["message"]);
+}
+
+function errorOnAjax() {
+    console.log("error");
+}
