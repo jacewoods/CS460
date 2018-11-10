51815ba6e46cc211019cd3a3cd943dd56786c8b3
Jace Woods <jwoods17@mail.wou.edu>
2018-11-10 02:24:08 -0800

    code now uses Giphy API to translate words to stickers, API Key is hidden

---
+++ homework7/GiphyProj/GiphyProj/Controllers/WordController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace GiphyProj.Controllers
+{
+    public class WordController : Controller
+    {
+        // GET: Hello
+        public JsonResult WordConvert(string id)
+        {
+            var data = new
+            {
+                inputText = id
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FindGiphy(string id)
+        {
+            string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyAPI"];
+            var data2 = new WebClient().DownloadString("https://api.giphy.com/v1/stickers/translate?api_key=" + apiKey + "&s=" + id);
+
+            return Content(data2, "application/json");
+        }
+    }
+}
---
+++ homework7/GiphyProj/GiphyProj/Scripts/translator.js
@@ -0,0 +1,41 @@
+﻿console.log("In translator.js")
+
+
+
+$("#Request").bind('keypress', function (e) {
+    if (e.key === ' ' || e.key === 'Spacebar') {
+        var word = $("#Request").val().split(" ").slice(-1);
+        //console.log(word);
+
+        if (word == "cat") {
+            var source = "/Word/WordConvert/" + word;
+        }
+        else {
+            var source = "/Word/FindGiphy/" + word;
+        }
+        //console.log(source);
+    }
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: source,
+        success: displayData,
+        error: errorOnAjax
+    });
+
+});
+
+function displayData(info) {
+    //console.log(data);
+    if (info.inputText != null) {
+        $("#message").append(info.inputText + " ");
+    }
+    else if (info.data.type != "undefined") {
+        $("#message").append("<img src='" + info.data.images.original_still.url + "' style='height:100px; width:100px;'/>" + " ");
+    }
+}
+
+function errorOnAjax() {
+    console.log("error");
+}
