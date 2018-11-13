619ca62f139c34f9cebae94dbedf9dee081fbd76
Jace Woods <woodsjace@yahoo.com>
2018-11-13 01:21:12 -0800

    starter DAL/dB added

---
+++ homework8/HW8/HW8/DAL/AuctionContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using HW8.Models;
+
+namespace HW8.DAL
+{
+    public class AuctionContext : DbContext
+    {
+        public AuctionContext() : base("name=Auctions") { }
+
+        public virtual DbSet<Buyer> Buyers { get; set; }
+        public virtual DbSet<Bid> Bids { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<Seller> Sellers { get; set; }
+    }
+
+}
